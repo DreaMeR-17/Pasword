95089e242ba8478ea5d2f1ca00eadc6352b0c92a
DreaMeR <DreaMeR@DREAMER>
2024-07-16 11:26:00 +0600

    Добавьте файлы проекта.

---
+++ Pasword/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Pasword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pasword = "admin";
+            string userInput;
+
+            int triesCount = 3;
+
+            for (int i = 0; i < triesCount; i++)
+            {
+
+                Console.WriteLine("Введите пароль.");
+                userInput = Console.ReadLine();
+
+                if (userInput == pasword)
+                {
+                    Console.WriteLine("Вот все ваши секреты");
+                    break;
+                }
+                
+                else
+                {
+                    Console.WriteLine("Пароль неверный, попробуйте еще раз");
+                    Console.WriteLine($"Осталось {triesCount - i - 1} попытки");
+                }
+            }
+        }
+    }
+}
